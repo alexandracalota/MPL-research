2b2892916abadf0523d081149155ea3d0aa5b647
alexandracalota <alexandra.calota@hootsuite.com>
2022-02-07 22:03:44 +0200

    add text dataset and loader and models

---
+++ data/textDataset.py
@@ -0,0 +1,76 @@
+import json
+import numpy as np
+import os
+from PIL import Image
+import random
+from collections import Counter
+import torch
+from torch.utils.data import Dataset
+
+from utils.utils import truncate_seq_pair, numpy_seed
+import pandas as pd
+
+class TextDataset(Dataset):
+    def __init__(self, csv_path, tokenizer, vocabulary, args, text_aug1='none', text_aug2='none'):
+        self.data = pd.read_csv(csv_path)
+
+        self.num_classes = self.data['Label'].unique().shape[0]
+        print(f'Loaded {self.data.shape[0]} examples equally distributed from {self.num_classes} classes.')
+
+        self.args = args
+        self.tokenizer = tokenizer
+        self.vocab = vocabulary
+        self.max_seq_len = args.max_seq_len
+        self.text_start_token = ["[CLS]"] if args.model != "mmbt" else ["[SEP]"]
+        self.text_aug1 = text_aug1
+        self.text_aug2 = text_aug2
+
+    def __len__(self):
+        return self.data.shape[0]  # len(self.data)
+
+    def get_sentence_and_segment(self, text):
+        sentence = (
+            self.text_start_token
+            + self.tokenizer(text)[
+                : (self.args.max_seq_len - 1)
+            ]
+        )
+        segment = torch.zeros(len(sentence))
+
+        sentence = torch.LongTensor(
+            [
+                self.vocab.stoi[w] if w in self.vocab.stoi else self.vocab.stoi["[UNK]"]
+                for w in sentence
+            ]
+        )
+
+        return sentence, segment
+
+    def _get_augmented_sentence(self, row, text_aug):
+        if text_aug == 'none':
+            text = row['Text']
+            return self.get_sentence_and_segment(text)
+
+        elif text_aug == 'back_translate':
+            text = random.choice(row['back_translate'])
+            return self.get_sentence_and_segment(text)
+
+        elif text_aug == 'eda':
+            text = random.choice(row['eda'])
+            return self.get_sentence_and_segment(text)
+
+        else:
+            raise NotImplementedError(f'{text_aug} is not a valid augmentation')
+
+    def __getitem__(self, index):
+        row = self.data.iloc[index]
+
+        sentence1, segment1 = self._get_augmented_sentence(row, self.text_aug1)
+        sentence2, segment2 = self._get_augmented_sentence(row, self.text_aug2)
+
+        label_string = row['Label']
+        label_int = self.args.labels.index(label_string)
+        label = torch.LongTensor([label_int])
+
+        return sentence1, segment1, sentence2, segment2, label
+
---
+++ data/textLoader.py
@@ -0,0 +1,161 @@
+import functools
+import json
+import os
+from collections import Counter
+
+import pandas as pd
+
+import torch
+import torchvision.transforms as transforms
+from pytorch_pretrained_bert import BertTokenizer
+
+from data.myDataset import JsonlDataset
+from data.vocab import Vocab
+
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data.distributed import DistributedSampler
+from data.textDataset import TextDataset
+
+
+TEXT_SIZE = 224
+
+def get_vocab(args):
+    vocab = Vocab()
+    bert_tokenizer = BertTokenizer.from_pretrained(
+        args.bert_model, do_lower_case=True
+    )
+    vocab.stoi = bert_tokenizer.vocab
+    vocab.itos = bert_tokenizer.ids_to_tokens
+    vocab.vocab_sz = len(vocab.itos)
+
+    return vocab
+
+
+def get_labels_and_frequencies(path):
+    df = pd.read_csv(path)
+    label_freqs = Counter(df.Label)
+    print(f'Labels frequencies: {label_freqs}')
+
+    return list(label_freqs.keys()), label_freqs
+
+def get_datasets(args):    
+    tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case=True).tokenize
+
+    args.labels, args.label_freqs = get_labels_and_frequencies(
+        os.path.join(args.data_path, args.task, f"{args.train_file}.csv")
+    )
+    vocab = get_vocab(args)
+    args.vocab = vocab
+    args.vocab_sz = vocab.vocab_sz
+    args.n_classes = len(args.labels)
+
+    labeled_dataset = TextDataset(
+        os.path.join(args.data_path, args.task, f"{args.train_file}.csv"),
+        tokenizer,
+        vocab,
+        args,
+        text_aug1=args.text_soft_aug,
+        text_aug2='none'
+    )
+
+    args.train_data_len = len(labeled_dataset)
+
+    unlabeled_dataset = TextDataset(
+        os.path.join(args.data_path, args.task, f"unlabeled.csv"),
+        tokenizer,
+        vocab,
+        args,
+        text_aug1=args.text_soft_aug,
+        text_aug2=args.text_hard_aug
+    )
+
+    dev_dataset = TextDataset(
+        os.path.join(args.data_path, args.task, f"dev.csv"),
+        tokenizer,
+        vocab,
+        args,
+        text_aug1='none',
+        text_aug2='none'
+    )
+
+    test_dataset = TextDataset(
+        os.path.join(args.data_path, args.task, f"test.csv"),
+        tokenizer,
+        vocab,
+        args,
+        text_aug1='none',
+        text_aug2='none'
+    )
+    
+    return labeled_dataset, unlabeled_dataset, dev_dataset, test_dataset
+
+
+def prepare_text_segment_mask(batch, index_text, index_seg):
+    lens = [len(row[index_text]) for row in batch]
+    bsz, max_seq_len = len(batch), max(lens) if TEXT_SIZE is None else TEXT_SIZE
+
+    mask_tensor = torch.zeros(bsz, max_seq_len).long()
+    sentence_tensor = torch.zeros(bsz, max_seq_len).long()
+    segment_tensor = torch.zeros(bsz, max_seq_len).long()
+
+    for i_batch, (input_row, length) in enumerate(zip(batch, lens)):
+        if length > max_seq_len:
+            length = max_seq_len
+        tokens, segment = input_row[index_text], input_row[index_seg]
+        sentence_tensor[i_batch, :length] = tokens[:length]
+        segment_tensor[i_batch, :length] = segment[:length]
+        mask_tensor[i_batch, :length] = 1
+    
+    return sentence_tensor, segment_tensor, mask_tensor
+
+def collate_function(batch):
+    sentence_tensor1, segment_tensor1, mask_tensor1 = prepare_text_segment_mask(batch, 0, 1)
+    sentence_tensor2, segment_tensor2, mask_tensor2 = prepare_text_segment_mask(batch, 2, 3)
+
+    tgt_tensor = torch.cat([elem[4] for elem in batch]).long()
+
+    return sentence_tensor1, segment_tensor1, mask_tensor1, sentence_tensor2, segment_tensor2, mask_tensor2, tgt_tensor
+
+
+def get_data_loaders(args):
+    labeled_dataset, unlabeled_dataset, dev_dataset, test_dataset = get_datasets(args)
+    
+    if args.local_rank == 0:
+        torch.distributed.barrier()
+
+    labeled_loader = DataLoader(
+        labeled_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.n_workers,
+        collate_fn=collate_function,
+        drop_last=True
+    )
+    
+    unlabeled_loader = DataLoader(
+        unlabeled_dataset,
+        shuffle=True,
+        batch_size=args.batch_size*args.mu,
+        num_workers=args.num_workers,
+        collate_fn=collate_function,
+        drop_last=True
+    )
+    
+    dev_loader = DataLoader(
+        dev_dataset,
+        batch_size=args.batch_size*(args.mu+1),
+        shuffle=False,
+        num_workers=args.n_workers,
+        collate_fn=collate_function,
+    )
+    
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=args.batch_size*(args.mu+1),
+        shuffle=False,
+        num_workers=args.n_workers,
+        collate_fn=collate_function,
+    )
+
+    return labeled_loader, unlabeled_loader, dev_loader, test_loader
+
