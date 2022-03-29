a15be9dd4092dc2d990018c33718f20c588e90ff
Alexandra Calota <alexandracalota@Alexandras-MacBook-Pro.local>
2022-03-30 00:17:06 +0300

    fix logging

--- text_main.py
+++ text_main.py
@@ -201,7 +201,7 @@ def train_loop(args, labeled_loader, unlabeled_loader, test_loader,
             s_logits = student_model(s_texts, s_masks, s_segments)
 
             if step % 100 == 0:
-                logger.info(f's_texts shape: {s_texts.shape} s_masks shape: {s_masks.shape} s_segments shape: {s_segments.shape}')
+                logger.info(f'\ns_texts shape: {s_texts.shape} s_masks shape: {s_masks.shape} s_segments shape: {s_segments.shape}')
                 logger.info(f'logits shape: {s_logits.shape} targets shape: {targets.shape}')
                 logger.info(s_logits)
                 logger.info(targets)
